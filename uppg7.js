628ea0279eff7871a7a7923fae2e90bf877d69c4
Emilia74 <emiliatasci@gmail.com>
2025-05-31 00:57:03 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -5,17 +5,12 @@ function uppg7() {
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
-function summa(tal1, tal2) {
-
-  return tal1 + tal2;
+   
+function sum(a, b) {
+  return a + b;
 }
 
-
-let resultat = summa(5, 10);
-
-
-console.log(resultat);
- 
+return sum(5, 10);
 }
 
 module.exports = { uppg7 };
