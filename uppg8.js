0a4a06b1fe07e99c5a7c6fe294794d9e9fe73975
Emilia74 <emiliatasci@gmail.com>
2025-05-31 00:49:16 +0200

    Update uppg8.js

--- uppg8.js
+++ uppg8.js
@@ -10,7 +10,22 @@ function uppg8(){
 //  över 30 år som skrivs ut i konsolen
 
 // anropa funktionen och skicka med arrayen som argument
+let personer = [
+  { name: "Anna", age: 25 },
+  { name: "Björn", age: 32 },
+  { name: "Carla", age: 41 },
+  { name: "Daniel", age: 28 },
+  { name: "Elin", age: 35 }
+];
 
+function skrivPersonerOver30(personArray) {
+ 
+  for (let i = 0; i < personArray.length; i++) {
+    if (personArray[i].age > 30) {
+      console.log(personArray[i].name);
+    }
+  }
+}
 
 }
 
